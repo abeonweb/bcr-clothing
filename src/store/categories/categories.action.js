54588fd54f69dfa6b9a244b44ffee7b909863da4
abeonweb <bmospecific@yahoo.com>
2023-10-30 23:45:14 +0100

    added saga file with generator functions, updated code in shop component

--- src/store/categories/categories.action.js
+++ src/store/categories/categories.action.js
@@ -2,18 +2,8 @@ import { CATEGORIES_ACTIONS } from "./categories.types";
 import { createAction } from "../../utils/reducer/reducer.utils"
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
-export const fetchCategoriesStart = () => createAction(CATEGORIES_ACTIONS.FETCH_CATEGORIES_START) 
+export const fetchCategoriesStart = () => createAction(CATEGORIES_ACTIONS.FETCH_CATEGORIES_START);
 
-export const fetchCategoriesSuccess = (categoriesArray) => createAction(CATEGORIES_ACTIONS.FETCH_CATEGORIES_SUCCESS, categoriesArray) 
+export const fetchCategoriesSuccess = (categoriesArray) => createAction(CATEGORIES_ACTIONS.FETCH_CATEGORIES_SUCCESS, categoriesArray);
 
-export const fetchCategoriesFailed = (error) => createAction(CATEGORIES_ACTIONS.FETCH_CATEGORIES_FAILED, error ) 
-
-export const fetchCategoriesAsync = ()=> async (dispatch) => {
-    dispatch(fetchCategoriesStart());
-    try {
-        const categoriesArray = await getCategoriesAndDocuments('categories');
-        dispatch(fetchCategoriesSuccess(categoriesArray));
-    } catch(err) {
-        dispatch(fetchCategoriesFailed(err));
-    }
-}
+export const fetchCategoriesFailed = (error) => createAction(CATEGORIES_ACTIONS.FETCH_CATEGORIES_FAILED, error ); 
