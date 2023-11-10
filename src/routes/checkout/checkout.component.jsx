6482eeffa85a49c04f07ee5c2b3839b4702db600
abeonweb <bmospecific@yahoo.com>
2023-11-10 18:56:32 +0100

    created payment form and payment styles

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -7,6 +7,7 @@ import {
 import { selectCartTotal } from "../../store/cart/cart.selector";
 import { selectCartItems } from "../../store/cart/cart.selector";
 import "./checkout.styles.scss";
+import PaymentForm from "../../components/payment-form/Payment-form.component";
 
 const Checkout = () => {
   const dispatch = useDispatch();
@@ -52,6 +53,7 @@ const Checkout = () => {
         />
       ))}
       <span className="total">Total: ${cartTotal}</span>
+      <PaymentForm />
     </div>
   );
 };
